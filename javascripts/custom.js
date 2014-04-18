4bfc295f29f331285800ad8f608257dbf62478a8
Scott Weisman <scott@launchpadlab.com>
2014-04-18 09:03:49 -0500

    added ns holdings simple form api key

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,7 +3,7 @@
 $('#ajax-form').submit(function(){
   $.ajax({
     dataType: 'jsonp',
-    url: "http://getsimpleform.com/messages/ajax?form_api_token=8c7851cc798e21152db0fdb7bcdb7d74",
+    url: "http://getsimpleform.com/messages/ajax?form_api_token=ba95c5d3f45778e730af682a1d883fbf",
     data: $('#ajax-form').serialize()
   }).done(function() {
     //callback which can be used to show a thank you message
