8be03ec585fd6719a1e9e2542df5822df16f02ea
Scott <scott@launchpadlab.com>
2020-07-23 08:37:28 -0700

    updated contact form https

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,7 +3,7 @@
 $('#ajax-form').submit(function(){
   $.ajax({
     dataType: 'jsonp',
-    url: "http://getsimpleform.com/messages/ajax?form_api_token=ba95c5d3f45778e730af682a1d883fbf",
+    url: "https://getsimpleform.com/messages/ajax?form_api_token=ba95c5d3f45778e730af682a1d883fbf",
     data: $('#ajax-form').serialize()
   }).done(function() {
     //callback which can be used to show a thank you message
